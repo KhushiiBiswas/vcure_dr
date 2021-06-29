2e8d907dfd9fcf13f978f0209fc2b87f843cdd0d
gagan <gagandeepsingh9575@gmail.com>
2021-06-29 13:34:55 +0530

    LEFT SOMETHING

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:vcure_doctors/components/background.dart';
 import 'package:vcure_doctors/pages/Doctors_Panel/Doctors_FrontPage.dart';
 
 import 'package:vcure_doctors/pages/kyc.dart';
+import 'package:vcure_doctors/pages/slots/slots.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DoctorsFront(),
+      home: Slots(),
     );
   }
 }
--- lib/pages/slots/slots.dart
+++ lib/pages/slots/slots.dart
@@ -1,5 +1,7 @@
  import 'package:flutter/material.dart';
+import 'package:vcure_doctors/components/appointment_toogle.dart';
  import 'package:vcure_doctors/components/button1.0.dart';
+import 'package:vcure_doctors/components/location_drpdown.dart';
  import 'package:vcure_doctors/pages/demo.dart';
 
  class Slots extends StatefulWidget {
@@ -12,11 +14,132 @@
  class _SlotsState extends State<Slots> {
    @override
    Widget build(BuildContext context) {
-     return SingleChildScrollView(
-       child: Material(
-         child: Column(
-           children: [Text("")],
+     return Material(
+            child: SingleChildScrollView(
+         
+            child: Padding(
+              padding: const EdgeInsets.only(top: 100),
+              child: Column(
+              children: [
+               
+               Text("  Doctors Panel ",style: TextStyle(fontSize: 18),),
+               Padding(
+                 padding: const EdgeInsets.only(top: 50),
+                 child: Column(
+                   
+                   children: [
+                     Text("  Morning  "),
+                     Row(
+                       children:[
+                         Row(children: [
+                           Text("8 AM - 9 AM"),
+                           CustomToggle()
+                         ],),
+                         Row(children: [
+                           Text("9 AM - 10 AM"),
+                           CustomToggle()
+                         ],),
+                         
+                         ]
+                     ),
+                      Row(
+                       children:[
+                         Row(children: [
+                           Text("10 AM - 11 AM"),
+                           CustomToggle()
+                         ],),
+                         Row(children: [
+                           Text("11 AM - 12 AM"),
+                           CustomToggle()
+                         ],),
+                         
+                         ]
+                     )
+                     
+                   ],
+
+                 ),
+               ),
+               Padding(
+                 padding: const EdgeInsets.only(top: 50),
+                 child: Column(
+                   
+                   children: [
+                     Text("  Afternoon  "),
+                     Row(
+                       children:[
+                         Row(children: [
+                           Text("12 PM - 1 PM"),
+                           CustomToggle()
+                         ],),
+                         Row(children: [
+                           Text("1 PM - 2 PM"),
+                           CustomToggle()
+                         ],),
+                         
+                         ]
+                     ),
+                      Row(
+                       children:[
+                         Row(children: [
+                           Text("2 PM - 3 PM"),
+                           CustomToggle()
+                         ],),
+                         Row(children: [
+                           Text("3 PM - 4 PM"),
+                           CustomToggle()
+                         ],),
+                         
+                         ]
+                     )
+                     
+                   ],
+
+                 ),
+               ),
+               Padding(
+                 padding: const EdgeInsets.only(top: 50),
+                 child: Column(
+                   
+                   children: [
+                     Text("  Evening  "),
+                     Row(
+                       children:[
+                         Row(children: [
+                           Text("4 PM - 5 PM"),
+                           CustomToggle()
+                         ],),
+                         Row(children: [
+                           Text("5 PM - 6 PM"),
+                           CustomToggle()
+                         ],),
+                         
+                         ]
+                     ),
+                      Row(
+                       children:[
+                         Row(children: [
+                           Text("6 PM - 7 PM"),
+                           CustomToggle()
+                         ],),
+                         Row(children: [
+                           Text("7 PM - 8 PM"),
+                           CustomToggle()
+                         ],),
+                         
+                         ]
+                     )
+                     
+                   ],
+
+                 ),
+               )
+           ],
+           
+           ),
+            ),
          ),
-       )
      );
+     
      }
+ }
